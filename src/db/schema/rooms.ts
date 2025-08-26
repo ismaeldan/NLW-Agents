88689823998c6e5a40f53f1bc72db18b7d155188
Ismael <ismael.dantas@hotmail.com>
2025-08-25 23:20:52 -0300

    Criando o drizzle.config.ts

--- src/db/schema/rooms.ts
+++ src/db/schema/rooms.ts
@@ -1,6 +1,8 @@
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 
 export const rooms = pgTable('rooms', {
   id: uuid().primaryKey().defaultRandom(),
-  name: text().notNull()
+  name: text().notNull(),
+  description: text(),
+  createdAt: timestamp().defaultNow().notNull()
 })
